7f61ddfcea8a119bcc67e2e7ef6377886759f25d
williumchen <wichen@g.hmc.edu>
2016-02-15 15:50:01 -0800

    finished for loop

--- src/main/scala/internal/ForLoop.scala
+++ src/main/scala/internal/ForLoop.scala
@@ -1,21 +1,28 @@
-//package internal
-//
-///**
-// * the loop body should execute 5 times and print:
-// * 0
-// * 2
-// * 4
-// * 6
-// * 8
-// */
-//
-//object ForLoop extends App {
-//
-//  // define the new control-flow structure here 
-//  
-//  var i = 0;
-//  for_loop(i = 0)(i < 10)(i += 2) {
-//    println(i);
-//  }
-//
-//}
+package internal
+
+/**
+ * the loop body should execute 5 times and print:
+ * 0
+ * 2
+ * 4
+ * 6
+ * 8
+ */
+
+object ForLoop extends App {
+
+  // define the new control-flow structure here 
+  def for_loop(start: Unit)(condition: =>Boolean)(end: =>Unit)(function: =>Unit): Unit = {
+    if (condition) {
+      start
+      function
+      for_loop(end)(condition)(end)(function)
+    }
+  }
+  
+  var i = 0;
+  for_loop(i = 0)(i < 10)(i += 2) {
+    println(i);
+  }
+
+}
